8f0986643b9d642d08ad847d73ccc0881975eb51
Ruchita S <ruchitasonawale115@gmail.com>
2021-10-10 19:33:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".panel");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    //snap: 1 / (sections.length - 1),
+    // base vertical scrolling on how wide the container is so it feels more natural.
+    end: "+=3500",
+  },
+});
+//parallax instance for each scroll page
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene);
+
+var mercury = document.getElementById("mercury");
+var parallaxInstance = new Parallax(mercury);
+
+var venus = document.getElementById("venus");
+var parallaxInstance = new Parallax(venus);
+
+var mars = document.getElementById("mars");
+var parallaxInstance = new Parallax(mars);
+
+var earth = document.getElementById("earth");
+var parallaxInstance = new Parallax(earth);
+
+// arrow animations
+
+let arrow = document.querySelector(".arrow");
+
+if (arrow) {
+  gsap.to(arrow, { y: 12, ease: "power1.inOut", repeat: -1, yoyo: true });
+}
